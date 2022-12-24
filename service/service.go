aa72ff56a52a0e061d2c5e546d2b0046972409d3
devstackq <8akebaev@gmail.com>
2022-12-24 15:39:04 +0600

    estimate gifts

--- service/service.go
+++ service/service.go
@@ -1,8 +1,29 @@
 package service
 
+import "github.com/devstackq/das-santa.git/models"
+
 type Service struct {
+	gift *GiftSrv
+}
+
+type IService interface {
+	Ebash(data models.Map) error
 }
 
 func New() *Service {
 	return &Service{}
+}
+
+func (s Service) Ebash(data models.Map) error {
+	//1. estimate gift
+	gift := NewGift(data.Gifts)
+	gift.Estimate()
+
+	s.gift = gift //set estimate gifts
+
+	//2. find optimal path
+
+	//3. send gifts
+
+	return nil
 }
