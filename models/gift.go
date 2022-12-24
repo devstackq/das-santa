f0be942c68607587211ec6909558da767c72f9bf
devstackq <8akebaev@gmail.com>
2022-12-24 16:48:42 +0600

    sort by volume gift

--- models/gift.go
+++ models/gift.go
@@ -1,7 +1,72 @@
 package models
 
+import "sort"
+
 type Gift struct {
 	ID     int `json:"id"`
 	Weight int `json:"weight"`
 	Volume int `json:"volume"`
+}
+
+type EstimatationGifts struct {
+	//Optimal  EstimateGift
+	//ByVolume EstimateGift
+	//ByWeight EstimateGift
+	Gifts    []Gift
+	Optimal  [][]Gift
+	ByVolume [][]Gift
+	ByWeight [][]Gift
+}
+type EstimateGift struct {
+	Index      int
+	Gifts      []Gift
+	Min        int
+	Max        int
+	FirstIndex int
+	LastIndex  int
+}
+
+func (eg EstimatationGifts) SortByWeightAsc() {
+	temp := eg.Gifts
+
+	sort.SliceStable(temp, func(i, j int) bool {
+		return temp[i].Weight < temp[j].Weight
+	})
+	//eg.Gifts = data
+
+}
+func (eg EstimatationGifts) separateByVolume(data []Gift) {
+	result := [][]Gift{}
+	volume := 0
+
+	var temp []Gift
+
+	for _, gift := range data {
+		//slice, sum each index item, when gift[i].Volume+=;
+
+		if volume <= 100 {
+			temp = append(temp, gift)
+			volume += gift.Volume
+		} else {
+			result = append(result, temp)
+			volume = 0
+			temp = nil
+		}
+	}
+	eg.ByVolume = result
+}
+
+func (eg EstimatationGifts) SortByVolumeAsc() {
+	//eg.ByVolume = eg.Gifts
+	temp := eg.Gifts
+
+	sort.SliceStable(temp, func(i, j int) bool {
+		return temp[i].Volume < temp[j].Volume
+	})
+
+	eg.separateByVolume(temp)
+}
+
+func (eg EstimatationGifts) SortOptimal() {
+
 }
--- service/gift.go
+++ service/gift.go
@@ -15,7 +15,25 @@ func NewGift(gifts []models.Gift) *GiftSrv {
 	}
 }
 
-func (g *GiftSrv) Estimate() {
+func (g *GiftSrv) Estimate() models.EstimatationGifts {
 	log.Println("find estimate gifts")
+	//w/200 + v/100
+	optimal := models.EstimatationGifts{
+		Gifts:    g.Data,
+		Optimal:  [][]models.Gift{},
+		ByVolume: [][]models.Gift{},
+		ByWeight: [][]models.Gift{},
+	}
 
+	const maxWeight = 200 //12
+	const maxVolume = 100 //7
+
+	//1 sort, min max, all items;
+	//2 sort - each part gift; 7
+
+	optimal.SortByVolumeAsc()
+	optimal.SortByWeightAsc()
+	optimal.SortOptimal()
+
+	return optimal
 }
