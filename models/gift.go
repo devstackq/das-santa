11edaf4540357d146c067c3050a68f5559aa2f8a
devstackq <8akebaev@gmail.com>
2022-12-24 19:57:55 +0600

    sortir gift done

--- models/gift.go
+++ models/gift.go
@@ -1,17 +1,17 @@
 package models
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 type Gift struct {
-	ID     int `json:"id"`
-	Weight int `json:"weight"`
-	Volume int `json:"volume"`
+	ID     int     `json:"id"`
+	Weight float64 `json:"weight"`
+	Volume float64 `json:"volume"`
 }
 
 type EstimatationGifts struct {
-	//Optimal  EstimateGift
-	//ByVolume EstimateGift
-	//ByWeight EstimateGift
 	Gifts    []Gift
 	Optimal  [][]Gift
 	ByVolume [][]Gift
@@ -26,25 +26,59 @@ type EstimateGift struct {
 	LastIndex  int
 }
 
-func (eg EstimatationGifts) SortByWeightAsc() {
+const maxWeight = 200.0 //12
+const maxVolume = 100.0 //7
+
+type Weight struct {
+}
+type Volume struct {
+}
+type Optimal struct {
+}
+
+func NewOptimal() *Optimal {
+	return &Optimal{}
+}
+
+func (eg *EstimatationGifts) SortByWeightAsc() {
 	temp := eg.Gifts
 
 	sort.SliceStable(temp, func(i, j int) bool {
-		return temp[i].Weight < temp[j].Weight
+		return temp[i].Weight > temp[j].Weight
 	})
-	//eg.Gifts = data
+	eg.separateByWeight(temp)
 
 }
-func (eg EstimatationGifts) separateByVolume(data []Gift) {
+
+func (eg *EstimatationGifts) separateByWeight(data []Gift) {
 	result := [][]Gift{}
-	volume := 0
+	weight := 0.0
 
 	var temp []Gift
 
 	for _, gift := range data {
 		//slice, sum each index item, when gift[i].Volume+=;
+		if weight <= maxWeight && weight+gift.Weight <= maxWeight {
+			temp = append(temp, gift)
+			weight += gift.Weight
+		} else {
+			result = append(result, temp)
+			weight = 0
+			temp = nil
+		}
+	}
 
-		if volume <= 100 {
+	eg.ByWeight = result
+}
+func (eg *EstimatationGifts) separateByVolume(data []Gift) {
+	result := [][]Gift{}
+	volume := 0.0
+
+	var temp []Gift
+
+	for _, gift := range data {
+		//slice, sum each index item, when gift[i].Volume+=;
+		if volume <= maxVolume && volume+gift.Volume <= maxVolume {
 			temp = append(temp, gift)
 			volume += gift.Volume
 		} else {
@@ -56,17 +90,67 @@ func (eg EstimatationGifts) separateByVolume(data []Gift) {
 	eg.ByVolume = result
 }
 
-func (eg EstimatationGifts) SortByVolumeAsc() {
+func (eg *EstimatationGifts) SortByVolumeAsc() {
 	//eg.ByVolume = eg.Gifts
 	temp := eg.Gifts
 
 	sort.SliceStable(temp, func(i, j int) bool {
-		return temp[i].Volume < temp[j].Volume
+		return temp[i].Volume > temp[j].Volume
 	})
 
 	eg.separateByVolume(temp)
 }
 
-func (eg EstimatationGifts) SortOptimal() {
+func (o *Optimal) Sort()
 
+func (eg *EstimatationGifts) SortOptimal() {
+	type Optimal struct {
+		IndexGift int
+		Value     float64
+	}
+	temp := eg.Gifts
+	opts := []Optimal{}
+
+	//prepare data
+	for idx, gift := range temp {
+		diff := gift.Weight/maxWeight - gift.Volume/maxVolume
+		opt := Optimal{}
+		opt.IndexGift = idx
+		opt.Value = math.Abs(diff)
+
+		opts = append(opts, opt)
+	}
+	sort.SliceStable(opts, func(i, j int) bool {
+		return opts[i].Value < opts[j].Value
+	})
+	tmpGifts := make([]Gift, len(temp))
+
+	for idx, item := range opts {
+		tmpGifts[idx] = temp[item.IndexGift]
+	}
+
+	eg.separateByOptimal(tmpGifts)
+
+}
+
+func (eg *EstimatationGifts) separateByOptimal(data []Gift) {
+	result := [][]Gift{}
+	sumVolume := 0.0
+	sumWeight := 0.0
+
+	var temp []Gift
+
+	for _, gift := range data {
+		if sumWeight <= maxWeight && sumWeight+gift.Weight <= maxWeight || sumVolume <= maxVolume && sumVolume+gift.Volume <= maxVolume {
+			temp = append(temp, gift)
+			sumVolume += gift.Volume
+			sumWeight += gift.Weight
+		} else {
+			result = append(result, temp)
+			sumVolume = 0
+			sumWeight = 0
+			temp = nil
+		}
+	}
+	eg.Optimal = result
 }
--- service/gift.go
+++ service/gift.go
@@ -15,25 +15,22 @@ func NewGift(gifts []models.Gift) *GiftSrv {
 	}
 }
 
-func (g *GiftSrv) Estimate() models.EstimatationGifts {
-	log.Println("find estimate gifts")
-	//w/200 + v/100
-	optimal := models.EstimatationGifts{
-		Gifts:    g.Data,
-		Optimal:  [][]models.Gift{},
-		ByVolume: [][]models.Gift{},
-		ByWeight: [][]models.Gift{},
-	}
+type Sortir interface {
+	Sort([]models.Gift) [][]models.Gift
+}
 
-	const maxWeight = 200 //12
-	const maxVolume = 100 //7
+func (g *GiftSrv) Estimate() [][]models.Gift {
+	log.Println("find estimate gifts", len(g.Data))
 
-	//1 sort, min max, all items;
-	//2 sort - each part gift; 7
+	//opt := models.NewOptimal(g.Data)
+	//opt.Sort()
+	//
+	//vol := models.NewVolume(g.Data)
+	//vol.Sort()
 
-	optimal.SortByVolumeAsc()
-	optimal.SortByWeightAsc()
-	optimal.SortOptimal()
+	log.Println("sorted by volume", len(vol.Result))
+	//log.Println("sorted by weight", len(optimal.ByWeight))
+	log.Println("sorted by optimal", len(opt.Result))
 
-	return optimal
+	return opt.Result
 }
